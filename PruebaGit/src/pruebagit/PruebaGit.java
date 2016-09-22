39a067c98f1542dd8aa1fb5c878f380a47ca2663
CelsoL <xjamana@gmail.com>
2016-09-22 01:50:35 -0300

    Update PruebaGit.java

--- PruebaGit/src/pruebagit/PruebaGit.java
+++ PruebaGit/src/pruebagit/PruebaGit.java
@@ -17,6 +17,7 @@ public class PruebaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo :D");
+        private int numero;
     }
     
 }
